031118b2d9f023d36cedfa8cf8406acd32105ed5
Parth Patel <parth@Parths-MacBook-Pro.local>
2018-10-18 11:05:00 -0500

    Get User Address

--- MapKit_Example/MapScreenViewController.swift
+++ MapKit_Example/MapScreenViewController.swift
@@ -13,14 +13,17 @@ import CoreLocation
 class MapScreenViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     
     let locationManager = CLLocationManager()
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
     }
     
+    // MARK: Check Location Service is enabled or not
     func checkLocationServices() {
         if CLLocationManager.locationServicesEnabled() {
             setupLocationManager()
@@ -35,13 +38,11 @@ class MapScreenViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
+    // MARK: Check user authorization status
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
             case .authorizedWhenInUse:
-                mapView.showsUserLocation = true
-                centerViewOnUserLocation()
-                locationManager.startUpdatingLocation()
-                break
+                startTrackingUserLocation()
             case .authorizedAlways:
                 break
             case .denied:
@@ -55,27 +56,79 @@ class MapScreenViewController: UIViewController {
         }
     }
     
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    // MARK: Set user location in center of screen
     func centerViewOnUserLocation() {
         if let location = locationManager.location?.coordinate {
             let region = MKCoordinateRegion.init(center: location, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
             mapView.setRegion(region, animated: true)
         }
     }
+    
+    func getCenterLocation(for mapview:MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 
 }
 
 extension MapScreenViewController: CLLocationManagerDelegate {
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
-        mapView.setRegion(region, animated: true)
-        
-    }
+//    // MARK: Update user location when moving
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+//        mapView.setRegion(region, animated: true)
+//
+//    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
+    }
+}
+
+extension MapScreenViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        
+        // MARK: only request for location if user will move for longer distance
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [ weak self ] (placemarks, error) in
+            
+            //guard let self = self else { return }
+            
+            if let _ = error {
+                Alert.showAlert(on: self!, with: "Error in Geocode Location", message: error as! String)
+                return
+            }
+            guard let placemark = placemarks?.first else {
+                Alert.showAlert(on: self!, with: "Error in placemark", message: error as! String)
+                return
+            }
+         
+            let stateName = placemark.administrativeArea ?? ""
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+        
+            DispatchQueue.main.async {
+                self?.addressLabel.text = "\(streetNumber) \(streetName), \(stateName)"
+            }
+        }
     }
 }
 
